dc15d25218a6cb3a4f501190b5c1fb4285dd82db
Rodrigo Arrieta Candia <riarrieta@uc.cl>
2021-06-07 22:58:09 -0400

    - fixed type-piracy in plot recipes - plot recipes now return SVectors instead of Vectors

--- src/plotrecipes.jl
+++ src/plotrecipes.jl
@@ -1,3 +1,28 @@
+
+"""
+    struct PlotPoints
+
+Structure used for dispatching plot recipes.
+Its use also avoids the problem of type-piracy.
+"""
+struct PlotPoints end
+
+# plot a vector of points
+@recipe function f(::PlotPoints, pts::SVector{N1, SVector{N2, Float64}}) where {N1, N2}
+    if N2 == 2
+        xx = SVector{N1, Float64}(pt[1] for pt in pts)
+        yy = SVector{N1, Float64}(pt[2] for pt in pts)
+        return xx,yy
+    elseif N2 == 3
+        xx = SVector{N1, Float64}(pt[1] for pt in pts)
+        yy = SVector{N1, Float64}(pt[2] for pt in pts)
+        yy = SVector{N1, Float64}(pt[3] for pt in pts)
+        return xx,yy,zz
+    else
+        notimplemented()
+    end
+end
+
 # plot a hyperrectangle
 @recipe function f(rec::HyperRectangle{N}) where {N}
     seriestype := :path
@@ -9,7 +34,7 @@
         pt2 = rec.high_corner
         x1, x2 = pt1[1], pt2[1]
         y1, y2 = pt1[2], pt2[2]
-        [x1,x2,x2,x1,x1], [y1,y1,y2,y2,y1]
+        @series SVector(x1,x2,x2,x1,x1), SVector(y1,y1,y2,y2,y1)
     elseif N == 3
         seriestype := :path
         pt1 = rec.low_corner
@@ -18,13 +43,13 @@
         y1, y2 = pt1[2], pt2[2]
         z1, z2 = pt1[3], pt2[3]
         # upper and lower faces
-        @series [x1,x2,x2,x1,x1], [y1,y1,y2,y2,y1], [z1,z1,z1,z1,z1]
-        @series [x1,x2,x2,x1,x1], [y1,y1,y2,y2,y1], [z2,z2,z2,z2,z2]
+        @series SVector(x1,x2,x2,x1,x1), SVector(y1,y1,y2,y2,y1), SVector(z1,z1,z1,z1,z1)
+        @series SVector(x1,x2,x2,x1,x1), SVector(y1,y1,y2,y2,y1), SVector(z2,z2,z2,z2,z2)
         # lines connecting faces
-        @series [x1,x1], [y1,y1], [z1,z2]
-        @series [x2,x2], [y1,y1], [z1,z2]
-        @series [x2,x2], [y2,y2], [z1,z2]
-        @series [x1,x1], [y2,y2], [z1,z2]
+        @series SVector(x1,x1), SVector(y1,y1), SVector(z1,z2)
+        @series SVector(x2,x2), SVector(y1,y1), SVector(z1,z2)
+        @series SVector(x2,x2), SVector(y2,y2), SVector(z1,z2)
+        @series SVector(x1,x1), SVector(y2,y2), SVector(z1,z2)
     end
 end
 
@@ -78,49 +103,31 @@ end
     end
 end
 
-@recipe function f(el::LagrangeTriangle)
+@recipe function f(el::LagrangeTriangle{3})
     label --> ""
     linecolor --> :black
     seriestype := :line
-    vtx = vals(el)
-    for n in 1:3
-        is = n
-        ie = 1 + (n%3)
-        @series begin
-            [vtx[is],vtx[ie]]
-        end
-    end
+    n = vals(el)  # nodes
+    @series PlotPoints(), SVector(n[1], n[2], n[3], n[1])
+end
+@recipe function f(el::LagrangeTriangle{6})
+    label --> ""
+    linecolor --> :black
+    seriestype := :line
+    n = vals(el)  # nodes
+    # Reorder nodes for visualization purposes
+    @series PlotPoints(), SVector(n[1], n[4], n[2], n[5], n[3], n[6], n[1])
 end
 
 @recipe function f(el::LagrangeRectangle)
     label --> ""
-    vtx = vals(el)
-    for n in 1:4
-        is = n
-        ie = 1 + (n%4)
-        @series begin
-            [vtx[is],vtx[ie]]
-        end
-    end
+    n = vals(el)  # nodes
+    @series PlotPoints(), SVector(n[1], n[2], n[3], n[4], n[1])
 end
 
 @recipe function f(el::LagrangeLine)
-    vtx = vals(el)
-    [vtx[1],vtx[2]]
+    n = vals(el)  # nodes
+    @series PlotPoints(), SVector(n[1], n[2])
 end
 
-# plot a vector of points
-@recipe function f(pts::AbstractVector{<:SVector{N}}) where {N}
-    if N == 2
-        xx = [pt[1] for pt in pts]
-        yy = [pt[2] for pt in pts]
-        return xx,yy
-    elseif N == 3
-        xx = [pt[1] for pt in pts]
-        yy = [pt[2] for pt in pts]
-        zz = [pt[3] for pt in pts]
-        return xx,yy,zz
-    else
-        notimplemented()
-    end
-end
+
