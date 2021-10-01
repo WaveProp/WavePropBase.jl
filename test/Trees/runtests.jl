43f484f826a2c586b7c489d853f477f2f156dc60
Luiz M. Faria <maltezfaria@users.noreply.github.com>
2021-10-01 10:05:33 +0200

    test Trees module

--- test/Trees/runtests.jl
+++ test/Trees/runtests.jl
@@ -1,4 +1,4 @@
 using Test
 using SafeTestsets
 
-@safetestset "Clustertree tests" begin include("clustertree_test.jl") end
+@safetestset "Clustertree tests" begin     include("clustertree_test.jl") end
