067ec5072c18e10477d43f98d71c92752d1660fa
Luiz M. Faria <maltezfaria@gmail.com>
2022-12-28 09:45:57 -0600

    rename `Density` to `NystromDensity`

--- src/IntegralEquations/density.jl
+++ src/IntegralEquations/density.jl
@@ -1,31 +1,34 @@
 """
-    struct Density{T,S} <: AbstractVector{T}
+    struct NystromDensity{T} <: AbstractVector{T}
 
-Discrete density with values `vals` on an `AbstractMesh`
+Discrete density with values `vals` on the [`qnodes`](@ref) of a [`NystromMesh`](@ref)
 """
-struct Density{V,S<:AbstractMesh} <: AbstractVector{V}
+struct NystromDensity{V,S<:NystromMesh} <: AbstractVector{V}
     vals::Vector{V}
     mesh::S
 end
 
 # AbstractArray interface
-Base.size(σ::Density) = size(vals(σ))
-Base.getindex(σ::Density, args...) = getindex(vals(σ), args...)
-Base.setindex!(σ::Density, args...) = setindex!(vals(σ), args...)
-Base.similar(σ::Density) = Density(similar(vals(σ)), mesh(σ))
+Base.size(σ::NystromDensity) = size(vals(σ))
+Base.getindex(σ::NystromDensity, args...) = getindex(vals(σ), args...)
+Base.setindex!(σ::NystromDensity, args...) = setindex!(vals(σ), args...)
+Base.similar(σ::NystromDensity) = NystromDensity(similar(vals(σ)), mesh(σ))
 
-vals(σ::Density) = σ.vals
-mesh(σ::Density) = σ.mesh
+vals(σ::NystromDensity) = σ.vals
+mesh(σ::NystromDensity) = σ.mesh
 
-function Density(f::Function, X::NystromMesh)
+"""
+    NystromDensity(f::Function, X::NystromMesh)
+
+Return a `NystromDensity` with values `f(q)` at the [`qnodes`](@ref) of `X`.
+
+Note that the argument passsed to `f` is a [`QuadratureNode`](@ref), so that `f`
+may depend on quantities other than the [`coords`](@ref) of the quadrature node
+(such as the [`normal`](@ref) vector).
+
+See also: [`QuadratureNode`](@ref)
+"""
+function NystromDensity(f::Function, X::NystromMesh)
     vals = [f(dof) for dof in qnodes(X)]
-    return Density(vals, X)
-end
-
-function γ₀(f, X::NystromMesh)
-    return Density(x -> f(coords(x)), X)
-end
-
-function γ₁(f, X::NystromMesh)
-    return Density(dof -> f(coords(dof), normal(dof)), X)
+    return NystromDensity(vals, X)
 end
